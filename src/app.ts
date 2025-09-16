09e7986ce7110a6233346ee1c7cff483b6e1efc4
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-09-16 12:32:47 +0600

    add user model APi errors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import type { Application } from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 // routes import
 import healthCheckRouter from "./routes/healthCheckRouter.router.js";
@@ -8,8 +9,14 @@ import healthCheckRouter from "./routes/healthCheckRouter.router.js";
 const app: Application = express();
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "20kb" }));
+app.use(cookieParser());
 
 // routes declaration
 app.use("/api/v1/health-check", healthCheckRouter);
